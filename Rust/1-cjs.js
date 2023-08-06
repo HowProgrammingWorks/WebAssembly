d5b9f2674e23b834aeff45079365b4fbdee5cf21
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-07 01:42:06 +0300

    Implement loader for rust wasm modules

--- Rust/1-cjs.js
+++ Rust/1-cjs.js
@@ -2,17 +2,23 @@
 
 const fsp = require('node:fs').promises;
 
+const load = async (fileName, exports) => {
+  const wasm = await fsp.readFile(fileName);
+  const mod = await WebAssembly.compile(wasm);
+  const units = WebAssembly.Module.imports(mod);
+  const context = { wbg: {} };
+  for (const [i, { name }] of units.entries()) {
+    context.wbg[name] = exports[i];
+  }
+  return await WebAssembly.instantiate(wasm, context);
+};
+
 (async () => {
   const callback = (res) => {
     console.log({ res });
   };
 
-  const context = {};
-  const wasm = await fsp.readFile('./pkg/example_bg.wasm');
-  const mod = await WebAssembly.compile(wasm);
-  const [unit] = WebAssembly.Module.imports(mod);
-  context[unit.module] = { [unit.name]: callback };
-  const example = await WebAssembly.instantiate(wasm, context);
+  const example = await load('./pkg/example_bg.wasm', [callback]);
 
   const sum = example.instance.exports.add(3, 7);
   console.log({ sum });
