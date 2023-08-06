156d3804b08ebb52d03d4b6ba675236b5fa2afd9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-06 17:33:18 +0300

    Add callback to AssemblyScript example

--- AssemblyScript/1-cjs.js
+++ AssemblyScript/1-cjs.js
@@ -3,8 +3,19 @@
 const fsp = require('node:fs').promises;
 
 (async () => {
+  const context = {
+    imports: {
+      callback: (res) => {
+        console.log({ res });
+      },
+    },
+  };
+
   const wasm = await fsp.readFile('./example.wasm');
-  const example = await WebAssembly.instantiate(wasm);
+  const example = await WebAssembly.instantiate(wasm, context);
+
   const sum = example.instance.exports.add(3, 7);
-  console.log(sum);
+  console.log({ sum });
+
+  example.instance.exports.addCallback(3, 7);
 })();
--- AssemblyScript/example.ts
+++ AssemblyScript/example.ts
@@ -1,3 +1,13 @@
+declare namespace context {
+  @external("imports", "callback")
+  export function callback(res: i32): void;
+}
+
 export function add(a: i32, b: i32): i32 {
   return a + b;
+}
+
+export function addCallback(a: i32, b: i32): void {
+  const res: i32 = a + b;
+  context.callback(a + b);
 }
