df378c000855d3cfc2749f0d046d7b043e5ffe18
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-07 01:09:47 +0300

    Get wasm import introspection

--- Rust/2-esm.mjs
+++ Rust/2-esm.mjs
@@ -1,6 +1,17 @@
 import fsp from 'node:fs/promises';
 
+const callback = (res) => {
+  console.log({ res });
+};
+
+const context = {};
 const wasm = await fsp.readFile('./pkg/example_bg.wasm');
-const example = await WebAssembly.instantiate(wasm);
+const mod = await WebAssembly.compile(wasm);
+const [unit] = WebAssembly.Module.imports(mod);
+context[unit.module] = { [unit.name]: callback };
+const example = await WebAssembly.instantiate(wasm, context);
+
 const sum = example.instance.exports.add(3, 7);
-console.log(sum);
+console.log({ sum });
+
+example.instance.exports.add_callback(3, 7);
--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -1,6 +1,16 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+extern {
+  pub fn callback(res: i32);
+}
+
+#[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
   return a + b;
+}
+
+#[wasm_bindgen]
+pub fn add_callback(a: i32, b: i32) {
+  callback(a + b)
 }
