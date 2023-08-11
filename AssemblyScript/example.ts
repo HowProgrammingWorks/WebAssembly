83f1c1733d990e8db304dc4fd1cde7c89c3a9bb4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2023-08-11 11:37:05 +0300

    Improve AssemblyScript example

--- AssemblyScript/example.ts
+++ AssemblyScript/example.ts
@@ -1,13 +1,21 @@
-declare namespace context {
-  @external("imports", "callback")
-  export function callback(res: i32): void;
+declare namespace example {
+  @external("example", "addCallback")
+  export function addCallback(res: i32): void;
+
+  @external("example", "subCallback")
+  export function subCallback(res: i32): void;
 }
 
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
 
-export function addCallback(a: i32, b: i32): void {
+export function addAsync(a: i32, b: i32): void {
   const res: i32 = a + b;
-  context.callback(a + b);
+  example.addCallback(res);
+}
+
+export function subAsync(a: i32, b: i32): void {
+  const res: i32 = a - b;
+  example.subCallback(res);
 }
